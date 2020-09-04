f477fe98d6a939616e232e9d5fca0722a7edbb32
mengqiDxiaofeifei <mengqiDxiaofeifei>
2020-09-04 14:11:07 +0800

    登录,注册功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
-var html;	  
+var html;
+var createVideo;	  
 		$.ajax({
 		         url: "/api/getUser",
 				 method: "GET",
@@ -16,6 +17,36 @@ var html;
 		         }
 		}) 
 		$("#userLabel").html(html);
+		$.ajax({
+		         url: "/api/vi/video",
+				 method: "GET",
+				 async: false,
+		         success: function (resp) {//请求数据并返回结果给success,是一个对象，类似python的字典。回调函数。data只是一个函数的参数，跟上面的data不一样
+		             console.log(resp.data.records);
+					if(resp.code === 200){
+						var list = resp.data.records;
+								$.each(list,function(i,item){
+								
+								createVideo +=   " <div class=\"col-md-3 col-sm-4 col-xs-6\"><div class=\"video-item\">\n" +
+								"                            <div class=\"thumb-wrap\">\n" +
+								"                                <img src='"+item.image+"' alt=\"Movie Thumb\">\n" +
+								"                                <span class=\"rating\">9.2</span>\n" +
+								"                                <div class=\"thumb-hover\">\n" +
+								"                                    <a class=\"play-video\" href=\"https://www.youtube.com/watch?v=5cY5PHE4x_g\"><i class=\"fa fa-play\"></i></a>\n" +
+								"                                </div>\n" +
+								"                            </div>\n" +
+								"                            <div class=\"video-details\">\n" +
+								"                                <h4 class=\"video-title\"><a href=\"movie-detail.html\">'"+item.title+"'</a></h4>\n" +
+								"                                <p class=\"video-release-on\">'"+item.uploadTime+"'</p>\n" +
+								"                            </div>\n" +
+								"                        </div></div>";
+								
+								$("#video-carousel").html(createVideo); 
+								
+									}); 
+							}
+		     }
+		}) 
 		 function loginOut(){
 			$.ajax({
 			         url: "/api/logout",
@@ -34,3 +65,6 @@ var html;
 				 window.location.href ="login.html";
 			 }
 		 }
+//最新视频
+		
+	
--- assets/js/login.js
+++ assets/js/login.js
@@ -131,7 +131,7 @@ function sendSmsBtn(obj) {
 					}else{
 						alert(data.message)
 					}
-				  }
+			}
 		})
 	  }else{
 		  $("#code").val("");
